9e25af3dfbf4e9db29e8e1f3ab99403fb23d688c
EamonKelly <144712664+EamonKelly@users.noreply.github.com>
2024-04-26 15:57:25 +0000

    we did it!!!!!!!!!!

--- script/main.js
+++ script/main.js
@@ -3,9 +3,8 @@ for (let i = 1; i <= 10; i++) {
     console.log(i * i);
     
 }
-for(let i=5;!i>=0; i--) {
-    console.log(i-1)
+console.log("task 2")
+for(let i=5; i>=0; i--) {
+    console.log(i)
     console.log("BLAST OFF")
 }
-
-console.log("task 2")
