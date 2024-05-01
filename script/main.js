9cbac801c1a47c1dd7efc97aa89d96ca9fa706d0
EamonKelly <144712664+EamonKelly@users.noreply.github.com>
2024-05-01 15:38:06 +0000

    fixed task 2

--- script/main.js
+++ script/main.js
@@ -6,8 +6,8 @@ for (let i = 1; i <= 10; i++) {
 console.log("task 2")
 for(let i=5; i>=0; i--) {
     console.log(i)
-    console.log("BLAST OFF")
 }
+console.log("BLAST OFF")
 
 console.log("task 3")
 for (let i = 1; i <= 50; i+=2) {
